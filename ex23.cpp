564052aa89d2e89adf7039844e3f49bb5ddc10a1
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2018-10-16 11:16:53 +0700

    test

---
+++ ex23.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <string.h>
+#include <cstring>
+ #include <iostream>
+int main(){
+  //char str[20];
+  printf("Nhap chuoi: ");
+  //gets(str);
+ // printf("Chuoi da nhap la: %s",str);
+  //printf("\nChuoi chu thuong la: %s",strupr(str));
+  std::cout << toupper('h');
+  return 0;
+}
