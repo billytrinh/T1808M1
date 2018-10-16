ca6836030279e7bd7f1117ce92e8daa420a46b2a
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2018-10-16 11:28:07 +0700

    delele printf code

--- ex23.cpp
+++ ex23.cpp
@@ -3,11 +3,5 @@
 #include <cstring>
  #include <iostream>
 int main(){
-  //char str[20];
-  printf("Nhap chuoi: ");
-  //gets(str);
- // printf("Chuoi da nhap la: %s",str);
-  //printf("\nChuoi chu thuong la: %s",strupr(str));
-  std::cout << toupper('h');
-  return 0;
+
 }
