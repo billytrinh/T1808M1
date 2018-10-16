d02532948d387fb9ec6c10831d3c953faabb2f2a
pstuan <tuandvth1808027@fpt.ed.vn>
2018-10-16 11:42:39 +0700

    Update ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	int n;
+	printf ("\nnhap so tu nhien n:");
+	scanf ("%d", &n);
+	printf ("cac so tu nhien nho hon %d la :",n);
+	for (int i=0;i<n;i++)
+	  if (i<n)
+	     printf ("%d\t", i);
+	     
+		 
+return 0;
+	
+}
