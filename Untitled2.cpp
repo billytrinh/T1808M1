c6dd921dc43eab3cbb0381c174b84d819193eea0
impercyjr <impercyjr@github.com>
2018-10-16 11:26:01 +0700

    Update Untitled2.cpp

---
+++ Untitled2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("nhap so can kiem tra n=\n");
+	scanf("%d",&n);
+		
+	for ( int i  = 2 < (n-1) ; i==i+1 )
+		{
+		}if ( n%i == 0 ) 
+		{ printf ("khong la so nguyen to\n",n);
+		break;
+		}
+		else {
+		Printf("la so nguyen to");}
+}
