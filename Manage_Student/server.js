ffd11642ea0ff7a3cc55e875eaf2c41effaf6639
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2018-12-27 11:20:08 +0700

    them xua xoa lop hoc

--- Manage_Student/server.js
+++ Manage_Student/server.js
@@ -19,10 +19,63 @@ MongoClient.connect(url, function (err, db) {
     console.log('Connected');
 
     // do some work here with the database.
-    
+
     //Close connection
    	// db.close();
-  }
+
+   	// Them 1 lop hoc
+   	var collection = db.db();
+   	var lophoc = collection.collection("lophoc");
+   	var lophoc1 = {
+   		ma_lop: 't1808m1',
+   		ten_lop: 'T1808M1',
+   		phong_hoc: '208'
+   	};
+   	/*
+   	lophoc.insert([lophoc1],function(err,result){
+   		if(err){
+   			console.log("khong thanh cong");
+   		}else{
+   			console.log("them lop thanh cong");
+   		}
+   	});
+   	*/
+
+   	// danh sach lop hoc
+   	/*
+ 	lophoc.find({ma_lop: 't1808m1',ten_lop:'T1808M1'})
+ 		.toArray(function (err, result) {
+	 		if(err){
+	 			console.log("khong ra du lieu");
+	 		}else{
+	 			console.log(result);
+	 		}
+  	});
+  	*/
+  	// update du lieu
+  	/*
+  	lophoc.update({ma_lop:'t1808m1'}, {$set: {ten_lop: 'lop hoc nang cao'}}, 
+  		function (err, numUpdated) {
+	  if (err) {
+	    console.log(err);
+	  }  else {
+	    console.log('cap nhat thanh cong');
+	  }
+	});
+	*/
+	// delete 
+	/*
+	lophoc.deleteMany({ma_lop:'t1808m1',ten_lop:'lop hoc nang cao'}, 
+		function(err, obj) {
+	    if (err){
+	    	console.log('chua xoa dc')
+	    }else{
+	    	console.log("xoa thanh cong");
+	    }
+	    
+	  });
+	  */
+ }
 });
 
 
