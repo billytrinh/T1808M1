4f3e64f6f30b22ae293306f43ceaab1e629729d0
SangDzai <sangptth1808011@fpt.edu.vn>
2019-02-19 16:35:13 +0700

    a

---
+++ MongoDB/sever.js
@@ -0,0 +1,110 @@
+
+var express = require("express");
+var app = express();
+app.use(express.static('public'));
+app.listen("3003",function(){
+	console.log("server is running");
+});
+
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+
+var url = 'mongodb://pts:sangcoi20@ds042698.mlab.com:42698/data';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error');
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connected');
+
+    // do some work here with the database.
+
+    //Close connection
+   	// db.close();
+
+   	// Them 1 lop hoc
+   	var collection = db.db();
+   	var lophoc = collection.collection("lophoc");
+   	var sinhvien = collection.collection("sinhvien");
+   	// var lophoc1 = {
+   	// 	ma_lop: 't1808m1',
+   	// 	ten_lop: 'T1808M1',
+   	// 	phong_hoc: '208'
+   	// };
+   	/*
+   	lophoc.insert([lophoc1],function(err,result){
+   		if(err){
+   			console.log("khong thanh cong");
+   		}else{
+   			console.log("them lop thanh cong");
+   		}
+   	});
+   	*/
+
+   	// danh sach lop hoc
+   	/*
+ 	lophoc.find({ma_lop: 't1808m1',ten_lop:'T1808M1'})
+ 		.toArray(function (err, result) {
+	 		if(err){
+	 			console.log("khong ra du lieu");
+	 		}else{
+	 			console.log(result);
+	 		}
+  	});
+  	*/
+  	// update du lieu
+  	/*
+  	lophoc.update({ma_lop:'t1808m1'}, {$set: {ten_lop: 'lop hoc nang cao'}}, 
+  		function (err, numUpdated) {
+	  if (err) {
+	    console.log(err);
+	  }  else {
+	    console.log('cap nhat thanh cong');
+	  }
+	});
+	*/
+	// delete 
+	/*
+	lophoc.deleteMany({ma_lop:'t1808m1',ten_lop:'lop hoc nang cao'}, 
+		function(err, obj) {
+	    if (err){
+	    	console.log('chua xoa dc')
+	    }else{
+	    	console.log("xoa thanh cong");
+	    }
+	    
+	  });
+	  */
+
+	app.get("/list_class",function(req,res){
+		lophoc.find().toArray(function (err, result) {
+	 		if(err){
+	 			res.send({
+	 				status: 0,
+	 				message: 'Fail'
+	 			});
+	 		}else{
+	 			//console.log(result);
+	 			res.send({
+	 				status: 1,
+	 				message: 'Success',
+	 				data: result
+	 			});
+	 		}
+  		});
+	});  
+	app.get("/delete-class",function(req,res){
+		var id =  req.query.id;
+		lophoc.deleteMany({_id:mongodb.ObjectId(id)}, 
+		function(err, obj) {
+	    if (err){
+	    	res.send("Fail");
+	    }else{
+	    	res.send("Success");
+	    }
+	    
+	  });
+	});
+ }
+});
--- nodejs/sever.js
+++ nodejs/sever.js
@@ -1,9 +1,60 @@
 var express = require("express");
-var app =express();
+var app = express();
+var server  = app.listen("3000",function() {
+	console.log("Server is running");
+});
+
+var storage = require("node-persist");
+storage.initSync({
+	dir: "data",
+	ttl: false
+});
+//storage.setItemSync("danhba",[]);
 
 app.use(express.static('public'));
 
-
-var server = app.listen("3003",function() {
-       console.log("server is running");
-	});
+app.get("/hello",function(req,res){
+	res.sendFile( __dirname + "/public/" + "layout.html" );
+});
+app.get("/them-danh-ba",function(req,res){
+	var name = req.query.user_name;
+	var tel = req.query.user_tel;
+	var danhba = storage.getItemSync("danhba");
+	var flag = true;
+	for (var i = 0; i < danhba.length; i++) {
+		if(name == danhba[i].name){
+			danhba[i].tel = tel;
+			flag = false;
+		}
+	}
+	if(flag){
+		danhba.push({
+			name: name,
+			tel: tel
+		});
+	}
+	storage.setItemSync("danhba",danhba);
+	res.send("done");
+});
+app.get("/danh-ba",function(req,res){
+	var danhba = storage.getItemSync("danhba");
+	var db = {
+		status: 1,
+	    message: "Success!",
+	    data: danhba
+	}
+	res.send(db);
+});
+app.get("/danh-ba/chi-tiet",function(req,res){
+	//var ID = req.query.id;
+	var db = {
+		status: 1,
+		    message: "Success!",
+		    data: 
+		    	{
+		    		name: "le van A",
+    				tel: "028292929"
+	    	 	}
+	}
+	res.send(db);
+});
